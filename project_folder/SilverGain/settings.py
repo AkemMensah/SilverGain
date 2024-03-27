4354ff69dcc28c58097cc6352d17794e5b6ee0db
AkemMensah <141175emmanuel@gmail.com>
2024-03-27 03:38:23 +0000

    settings for templates and static folder

--- project_folder/SilverGain/settings.py
+++ project_folder/SilverGain/settings.py
@@ -132,3 +132,6 @@ MEDIA_URL = '/uploads/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "auth.User"
